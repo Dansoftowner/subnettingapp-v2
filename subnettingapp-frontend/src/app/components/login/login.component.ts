7984eb925f5156500f2e37d31b7558a8aa336607
Daniel Gyoerffy <judalion777@gmail.com>
2025-05-07 19:43:23 +0200

    :sparkles: Login component

---
+++ subnettingapp-frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  error: string | null = null;
+
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router,
+    private translate: TranslateService
+  ) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) {
+      return;
+    }
+    const { email, password } = this.loginForm.value;
+    this.auth.login(email, password).subscribe({
+      next: () => this.router.navigate(['/']),
+      error: () => {
+        this.error = this.translate.instant('login.error');
+      },
+    });
+  }
+}
