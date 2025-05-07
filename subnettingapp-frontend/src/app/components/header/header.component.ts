6ff8da20a8f7f10ca57c7ee162432e02bd5105c3
Daniel Gyoerffy <judalion777@gmail.com>
2025-05-07 19:45:55 +0200

    :sparkles: User specific header toolbar

--- subnettingapp-frontend/src/app/components/header/header.component.ts
+++ subnettingapp-frontend/src/app/components/header/header.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user.model';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  user: User | null = null;
+
+  constructor(private auth: AuthService, private router: Router) {
+    this.user = this.auth.getCurrentUser();
+    auth.authChanged.subscribe((u) => (this.user = u));
+  }
+
+  logout(): void {
+    this.user = null;
+    this.auth.logout();
+    this.router.navigate(['/login']);
+  }
+}
